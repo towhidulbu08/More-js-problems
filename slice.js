6bab9fb81955fcd59e26938b8d600d14513bc8c8
towhidulbu08 <towhidulislam26121997@gmail.com>
2023-08-05 09:28:02 +0600

    splice

---
+++ slice.js
@@ -0,0 +1,8 @@
+//slice method use in an array return a part of array
+const friends=[12,23,34,56,76,89]
+var Portion=friends.slice(1,5)
+//slice method use in string return a part of string not an array
+const String='i am a good boy'
+var Char=String.slice(1,5)
+console.log(Portion,Char)
+console.log(friends,String)
---
+++ splice.js
@@ -0,0 +1,4 @@
+const friends=[23,34,343,87,89,73]
+//remove and insert new element at certain position in an array and return deleted elements and will change the original array
+const remove=friends.splice(1,2,10,20)
+console.log(friends,remove)
